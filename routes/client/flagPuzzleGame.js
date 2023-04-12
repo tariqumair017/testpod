cef16a5a6d0622bf5c6cc9c19ded888f8eb26d7e
unknown <rizwanramzan826@gmail.com>
2023-04-12 16:15:08 +0500

    update

--- routes/client/flagPuzzleGame.js
+++ routes/client/flagPuzzleGame.js
@@ -7,10 +7,10 @@ import asyncHandler from "express-async-handler";
 
 
 
-//Client Flag Puzzle Page
-router.get("/flag-puzzle", asyncHandler(async (req, res, next) => { 
-    res.render("Client/FlagPuzzleGame/FlagPuzzle"); 
-}));
+// //Client Flag Puzzle Page
+// router.get("/flag-puzzle", asyncHandler(async (req, res, next) => { 
+//     res.render("Client/FlagPuzzleGame/FlagPuzzle"); 
+// }));
 
 
 export default router;
