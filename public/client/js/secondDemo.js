1448a6c54df6fc1abd4a3eded411d497c0c9c363
unknown <rizwanramzan826@gmail.com>
2023-04-20 14:24:57 +0500

    test add image

--- public/client/js/secondDemo.js
+++ public/client/js/secondDemo.js
@@ -49,6 +49,8 @@ const menu_tigger = document.querySelectorAll(".menu-tigger")
 
 const menu_close = document.getElementById("menu-close")
 
+const quiz_image = document.querySelector(".quiz-image")
+
 
 
 
@@ -115,89 +117,12 @@ let completeTestDuration;
     
         options: [val.optionA, val.optionB, val.optionC, val.optionD],
 
-        hint : val.hint
+        hint : val.hint,
+
+        questionImg : val.questionImg
       }
     ));
 
-
-
-// var questions = [
-//   {
-//     numer: 1,
-//     img: "/client/img/bg/warningsign35.svg",
-//     question: "What does this sign mean?",
-//     answer: "The recommended speed limit for this curve is 35 mph.",
-//     hint: "The arrow represents the directions of traffic ahead.",
-//     discription:
-//       "This sign indicates a double curve ahead. The road ahead curves to the right and then to the left",
-//     options: [
-//       "There is a 35-degree curve ahead.",
-//       "The minimum speed for this curve is 35 mph.",
-//       "The maximum speed for this curve is 35 mph.",
-//       "The recommended speed limit for this curve is 35 mph.",
-//     ],
-//   },
-//   {
-//     numer: 2,
-//     img: "/client/img/bg/warningsign.svg",
-//     question: "This warning sign means?",
-//     answer: "There is a 35-degree curve ahead.",
-//     hint: "The arrow represents the directions of traffic ahead.",
-//     discription:
-//       "This sign indicates a double curve ahead. The road ahead curves to the right and then to the left",
-//     options: [
-//       "There is a 35-degree curve ahead.",
-//       "The maximum speed for this curve is 35 mph.",
-//       "The recommended speed limit for this curve is 35 mph.",
-//       "The minimum speed for this curve is 35 mph.",
-//     ],
-//   },
-//   {
-//     numer: 3,
-//     img: "/client/img/bg/warningsign35.svg",
-//     question: "What does this sign mean?",
-//     answer: "The recommended speed limit for this curve is 35 mph.",
-//     hint: "The arrow represents .",
-//     discription:
-//       "This sign indicates a double curve ahead. The road ahead curves to the right and then to the left",
-//     options: [
-//       "The recommended speed limit for this curve is 35 mph.",
-//       "There is a 35-degree curve ahead.",
-//       "The minimum speed for this curve is 35 mph.",
-//       "The maximum speed for this curve is 35 mph.",
-//     ],
-//   },
-//   {
-//     numer: 4,
-//     img: "/client/img/bg/warningsign.svg",
-//     question: "This warning sign means?",
-//     answer: "The recommended speed limit for this curve is 35 mph.",
-//     hint: "The arrow represent of traffic ahead.",
-//     discription:
-//       "This sign indicates a double curve ahead. The road ahead curves to the right and then to the left",
-//     options: [
-//       "There is a 35-degree curve ahead.",
-//       "The recommended speed limit for this curve is 35 mph.",
-//       "The minimum speed for this curve is 35 mph.",
-//       "The maximum speed for this curve is 35 mph.",
-//     ],
-//   },
-//   {
-//     numer: 5,
-//     img: "/client/img/bg/warningsign35.svg",
-//     question: "What does this sign mean?",
-//     answer: "The recommended speed limit for this curve is 35 mph.",
-//     hint: "The arrow represents the direction.",
-//     discription:
-//       "This sign indicates a double curve ahead. The road ahead curves to the right and then to the left",
-//     options: [
-//       "There is a 35-degree curve ahead.",
-//       "The minimum speed for this curve is 35 mph.",
-//       "The maximum speed for this curve is 35 mph.",
-//       "The recommended speed limit for this curve is 35 mph.",
-//     ],
-//   },
-// ];
 
 window.load = startQuiz();
 
@@ -229,6 +154,12 @@ function showQuetions(index) {
   `;
 
   let detail = questions[index].hint;
+
+  console.log(questions[index].questionImg)
+  if(questions[index].questionImg){
+  let option_images = `<img class="quiz-image-optional" src="/upload-images/${questions[index].questionImg}"}" />`
+    quiz_image.innerHTML = option_images
+  }
 
   let option_tag =
     '<div class="demoCustomLable slide-bottom1"><p class="option-number">A</p><p class="option-content">' +
