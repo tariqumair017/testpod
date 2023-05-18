0d370898fca31946dcdbf22e2676aa8007ffef0a
My Team Company <myteam192@gmail.com>
2023-05-18 12:21:25 +0500

    Pagination Client Side and Server Side complete

--- public/client/js/pagination.js
+++ public/client/js/pagination.js
@@ -1,29 +1,27 @@
-const element = document.querySelector(".pagination ul");
-let totalPages = 20;
-let page = 10;
+const element = document.querySelector(".pagination ul"); 
 
 //calling function with passing parameters and adding inside element which is ul tag
 element.innerHTML = createPagination(totalPages, page);
 function createPagination(totalPages, page) {
+  totalPages = Number(totalPages); 
+  page = Number(page); 
   let liTag = "";
   let active;
-  let beforePage = page - 1;
+  let beforePage = page;
   let afterPage = page + 1;
   if (page > 1) {
     //show the next button if the page value is greater than 1
-    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${
-      page - 1
-    })"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+    liTag += `<li class="btn prev" onclick="location='/dmv-test/${data[0].stateName}?page=${page - 1}'"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
   }
 
-  if (page > 2) {
-    //if page value is less than 2 then add 1 after the previous button
-    liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
-    if (page > 3) {
-      //if page value is greater than 3 then add this (...) after the first li or page
-      liTag += `<li class="dots"><span>...</span></li>`;
-    }
-  }
+  // if (page > 2) {
+  //   //if page value is less than 2 then add 1 after the previous button
+  //   liTag += `<li class="first numb" onclick="location='/dmv-test/${data[0].stateName}?page=${1}'"><span>1</span></li>`;
+  //   if (page > 3) {
+  //     //if page value is greater than 3 then add this (...) after the first li or page
+  //     liTag += `<li class="dots"><span>...</span></li>`;
+  //   }
+  // }
 
   // how many pages or li show before the current li
   if (page == totalPages) {
@@ -54,24 +52,27 @@ function createPagination(totalPages, page) {
       //else leave empty to the active variable
       active = "";
     }
-    liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
+    liTag += `<li class="numb ${active}" onclick="location='/dmv-test/${data[0].stateName}?page=${plength}'"><span>${plength}</span></li>`;
   }
 
-  if (page < totalPages - 1) {
-    //if page value is less than totalPage value by -1 then show the last li or page
-    if (page < totalPages - 2) {
-      //if page value is less than totalPage value by -2 then add this (...) before the last li or page
-      liTag += `<li class="dots"><span>...</span></li>`;
-    }
-    liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
-  }
+  // if (page < totalPages - 1) {
+  //   //if page value is less than totalPage value by -1 then show the last li or page
+  //   if (page < totalPages - 2) {
+  //     //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+  //     liTag += `<li class="dots"><span>...</span></li>`;
+  //   }
+  //   liTag += `<li class="last numb" onclick="location='/dmv-test/${data[0].stateName}?page=${totalPages}'"><span>${totalPages}</span></li>`;
+  // }
 
   if (page < totalPages) {
     //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${
-      page + 1
-    })"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+    liTag += `<li class="btn next" onclick="location='/dmv-test/${data[0].stateName}?page=${page + 1}'"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+  }
+  if(totalPages == 1){
+    document.querySelector(".pagination-div").classList.add("d-none")
   }
   element.innerHTML = liTag; //add li tag inside ul tag
+  
   return liTag; //reurn the li tag
+  
 }
